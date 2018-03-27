e3dd1c40320a9f6725596abd0778708d36a92843
迷彩风情 <sxdtzhaoxinguo@163.com>
2018-03-27 17:44:58 +0800

    优化代码结构

--- src/main/java/boss/portal/web/constant/ConstantKey.java
+++ src/main/java/boss/portal/web/constant/ConstantKey.java
@@ -9,5 +9,5 @@ package boss.portal.web.constant;
  */
 public class ConstantKey {
 
-    public static final String SIGNING_KEY = "Boss@Jwt!&Secret^#";
+    public static final String SIGNING_KEY = "spring-security-@Jwt!&Secret^#";
 }
